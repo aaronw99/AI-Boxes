13bf716f0da6e3731f5bae844ab4535f94d4471d
Aaron <aaron.t.wong@vanderbilt.edu>
2020-03-01 15:59:10 +0100

    graphsearch exercises

--- searchclient/searchclient_java/searchclient/GraphSearch.java
+++ searchclient/searchclient_java/searchclient/GraphSearch.java
@@ -8,7 +8,7 @@ public class GraphSearch {
 
     public static Action[][] search(State initialState, Frontier frontier)
     {
-        boolean outputFixedSolution = true;
+        boolean outputFixedSolution = false;
 
         if (outputFixedSolution) {
             //Exercise 1.1:
@@ -16,10 +16,25 @@ public class GraphSearch {
             //Try to solve a few levels such as SAD1 and SAD2 by hand and entering them below:
 
             return new Action[][] {
-                {Action.MoveS},
-                {Action.MoveE},
                 {Action.PushSS},
-                {Action.PullNE},
+                {Action.PushSS},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.MoveE},
+                {Action.PushSS}
             };
         } else {
             //Exercise 1.2:
@@ -38,14 +53,37 @@ public class GraphSearch {
             HashSet<State> explored = new HashSet<>();
 
             while (true) {
-
                 //Print a status message every 10000 iteration
                 if (++iterations % 10000 == 0) {
                     printSearchStatus(explored, frontier);
                 }
+                // If the frontier is empty, all possible states have been checked and none were
+                // the goal state. There is no solution.
+                if (frontier.isEmpty()) {
+                    return null;
+                }
 
-                //Your code here...
+                State cur = frontier.pop();
+                // If the state we just took from the frontier is the goal state, we have found
+                // the solution
+                if (cur.isGoalState()) {
+                    return cur.extractPlan();
+                }
+
+                // If we did not return in the previous statement, the current state is not the
+                // goal state. We keep track of it so as not to explore it again later.
+                explored.add(cur);
+                // We expand the current state to possibilities that follow and add them to the
+                // frontier for further exploration if they were not already explored or are not
+                // contained in the frontier already.
+                ArrayList<State> newStates = cur.getExpandedStates();
+                for (State newState : newStates) {
+                    if (!frontier.contains(newState) && !explored.contains(newState)) {
+                        frontier.add(newState);
+                    }
+                }
             }
+
         }
     }
 
@@ -56,6 +94,6 @@ public class GraphSearch {
         String statusTemplate = "#Explored: %,8d, #Frontier: %,8d, #Generated: %,8d, Time: %3.3f s\n%s\n";
         double elapsedTime = (System.nanoTime() - startTime) / 1_000_000_000d;
         System.err.format(statusTemplate, explored.size(), frontier.size(), explored.size() + frontier.size(),
-                          elapsedTime, Memory.stringRep());
+                elapsedTime, Memory.stringRep());
     }
 }
