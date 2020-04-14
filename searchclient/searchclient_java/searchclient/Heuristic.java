e96e596acfb5521a1f31998ff3482b31bcd8c91d
Aaron <aaron.t.wong@vanderbilt.edu>
2020-04-14 15:19:43 -0400

    uncommented Action and finished heuristic

--- searchclient/searchclient_java/searchclient/Heuristic.java
+++ searchclient/searchclient_java/searchclient/Heuristic.java
@@ -1,125 +1,121 @@
 package searchclient;
 
 import java.util.Comparator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
 
 public abstract class Heuristic
         implements Comparator<State>
 {
     private boolean[][] walls;
-
-    private int[] agentGoalRows;
-    private int[] agentGoalCols;
-    private int numAgents;
-
     private int[] boxGoalRows;
     private int[] boxGoalCols;
     private char[] boxGoalLetters;
     private int numGoalBoxes;
-
+    private Set<int[]> boxGoals;
     public Heuristic(State initialState)
     {
         // Here's a chance to pre-process the static parts of the level.
         walls = initialState.walls;
-        agentGoalRows = initialState.agentGoalRows;
-        agentGoalCols = initialState.agentGoalCols;
-        numAgents = this.agentGoalRows.length;
         boxGoalRows = initialState.boxGoalRows;
         boxGoalCols = initialState.boxGoalCols;
         boxGoalLetters = initialState.boxGoalLetters;
-        numGoalBoxes = this.boxGoalRows.length;
+        numGoalBoxes = boxGoalRows.length;
+        boxGoals = new HashSet<int[]>();
+        for(int i = 0; i < numGoalBoxes; i++) {
+            int[] arrayToAdd = {boxGoalRows[i], boxGoalCols[i], Character.getNumericValue(boxGoalLetters[i])};
+            boxGoals.add(arrayToAdd);
+        }
     }
 
-    public int h(State s)
-    {
+//    public int h(State s)
+//    {
+//        // first entry in each sublist is box, subsequent are goals
+//        List<List<int[]>> graph = new ArrayList<>();
+//        for(int i = 0; i < s.boxes.length; i++){
+//            for(int j = 0; j < s.boxes[0].length; j++){
+//                // once a box is found, add it and each of its goal to map
+//                if(Character.isLetter(s.boxes[i][j])){
+//                    List<int[]> box_to_goals = new ArrayList<>();
+//                    int[] box = new int[]{i,j};
+//                    box_to_goals.add(box);
+//                    for(int k = 0; k < numGoalBoxes; k++){
+//                        if(boxGoalLetters[k] == s.boxes[i][j]){
+//                            int[] goal = new int[]{boxGoalRows[k],boxGoalCols[k]};
+//                            box_to_goals.add(goal);
+//                        }
+//                    }
+//                    graph.add(box_to_goals);
+//                }
+//            }
+//        }
+//
+//        int cost = 0;
+//        // minimize total costs between boxes and goals,
+//        // but make sure boxes aren't aiming for same goals
+//        Set<int[]> usedGoals = new HashSet<>();
+//        for(List<int[]> cur : graph){
+//            int[] box = cur.get(0);
+//            int boxRow = box[0];
+//            int boxCol = box[1];
+//            int minDist = Integer.MAX_VALUE;
+//            int[] minGoal = new int[2];
+//            for(int i = 1; i < cur.size(); i++){
+//                int[] goal = cur.get(i);
+//                int goalRow = goal[0];
+//                int goalCol = goal[1];
+//                int manDist = Math.abs(goalRow-boxRow) + Math.abs(goalCol-boxCol);
+//                if(manDist<minDist){
+//                    minGoal[0] = goalRow;
+//                    minGoal[1] = goalCol;
+//                    if(usedGoals.contains(minGoal)){
+//                        continue;
+//                    }
+//                    minDist = manDist;
+//                }
+//            }
+//            cost += minDist;
+//            usedGoals.add(minGoal);
+//        }
+//        return cost;
+//    }
+
+
+    public int h(State s) {
         int cost = 0;
-        // check how close agents are to their goals
-        // euclidean distance for now, just to get an idea
-        // would be better to use distance among cells that can be travelled to
-        for(int i = 0; i < numAgents; i++){
-            int goalRow = agentGoalRows[i];
-            int goalCol = agentGoalCols[i];
-            int curRow = s.agentRows[i];
-            int curCol = s.agentCols[i];
-            cost += Math.sqrt(Math.pow((goalRow - curRow), 2) + Math.pow((goalCol - curCol),2));
+        Set<int[]> boxes = new HashSet<int[]>();
+        for (int i = 0; i < s.boxes.length; i++) {
+            for (int j = 0; j < s.boxes[0].length; j++) {
+                if (Character.isLetter(s.boxes[i][j])) {
+                    int[] arrayToAdd = {i, j, Character.getNumericValue(s.boxes[i][j])};
+                    boxes.add(arrayToAdd);
+                }
+            }
         }
+        Set<int[]> filledGoals = new HashSet<int[]>(boxes);
+        filledGoals.retainAll(boxGoals);
+        //remove goals already with a box from the set
+        boxGoals.removeAll(filledGoals);
+        //remove boxes already in their goal from the set
+        boxes.removeAll(filledGoals);
 
-        // check how close boxes are to their goals
-        // euclidean distance for now, just to get an idea
-        // would be better to use distance among cells that can be travelled to
-        for(int i = 0; i < numGoalBoxes; i++){
-            int goalRow = boxGoalRows[i];
-            int goalCol = boxGoalCols[i];
-            char goalLet = boxGoalLetters[i];
-            int[] closestBox = findClosestBox(s,goalRow,goalCol,goalLet);
-            int closestBoxRow = closestBox[0];
-            int closestBoxCol = closestBox[1];
-            cost += Math.sqrt(Math.pow((goalRow - closestBoxRow), 2) + Math.pow((goalCol - closestBoxCol),2));
+        for (int[] box : boxes) {
+            int overallDist = Integer.MAX_VALUE;
+            for (int[] boxGoal : boxGoals) {
+                if (boxGoal[2] == box[2]) {  //checking if the characters are the same
+                    int manDist = Math.abs(box[0] - boxGoal[0]) + Math.abs(box[1] - boxGoal[1]);
+                    overallDist = manDist < overallDist ? manDist : overallDist;
+                }
+            }
+            cost += overallDist;
         }
-
         return cost;
     }
 
-    private static int[] findClosestBox(State s, int row, int col, char let){
-        // check if the row and cal already has a box
-        if(s.boxes[row][col] == let){
-            return new int[]{row,col};
-        }
-
-        int maxRows = s.boxes.length;
-        int maxCols = s.boxes[0].length;
-
-        int up = row;
-        int down = row;
-        int left = col;
-        int right = col;
-
-        int foundRow = -1;
-        int foundCol = -1;
-
-        boolean found = false;
-        while(!found){
-            // check left and right border
-            for(int i = up; i <= down; i++){
-                char leftCell = s.boxes[i][left];
-                char rightCell = s.boxes[i][right];
-                if(leftCell == let){
-                    foundRow = i;
-                    foundCol = left;
-                    found=true;
-                    break;
-                }
-                if(rightCell == let){
-                    foundRow = i;
-                    foundCol = right;
-                    found=true;
-                    break;
-                }
-            }
-            // check top and bottom border
-            for(int i = left; i <= right; i++){
-                char topCell = s.boxes[up][i];
-                char botCell = s.boxes[down][i];
-                if(topCell == let){
-                    foundRow = up;
-                    foundCol = i;
-                    found=true;
-                    break;
-                }
-                if(botCell == let){
-                    foundRow = down;
-                    foundCol = i;
-                    found=true;
-                    break;
-                }
-            }
-
-            up = up>0 ? up-1 : up;
-            down = down<maxRows-1 ? down+1 : down;
-            left = left>0 ? left-1 : left;
-            right = right<maxCols-1 ? right+1 : right;
-        }
-        return new int[]{foundRow,foundCol};
-    }
 
     public abstract int f(State s);
 
